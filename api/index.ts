804a36fc376268b99700cf2f95c5feffc73898b4
David Johnston <davidajohnston2000@gmail.com>
2025-04-12 23:51:26 +0100

    Require JWT authentication

--- api/index.ts
+++ api/index.ts
@@ -2,9 +2,17 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const userInfoRouter = require('./routes/user-info');
+const { auth } = require('express-oauth2-jwt-bearer');
+
+const jwtCheck = auth({
+    audience: 'https://user-service.dajohnston.co.uk',
+    issuerBaseURL: 'https://dajohnston.eu.auth0.com/',
+    tokenSigningAlg: 'RS256'
+});
 
 const app = express();
 
+app.use(jwtCheck);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
