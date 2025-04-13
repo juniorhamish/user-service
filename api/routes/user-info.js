e35618e31ae51b03952ef57f5a427b8e865c4ae8
David Johnston <davidajohnston2000@gmail.com>
2025-04-13 01:37:09 +0100

    Get user info from Auth0

--- api/routes/user-info.js
+++ api/routes/user-info.js
@@ -1,9 +1,20 @@
+const {ManagementClient} = require('auth0');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res) {
-  console.log('GET /user-info');
-  res.json({ name: 'David' });
+const management = new ManagementClient({
+    clientId: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    domain: process.env.AUTH0_DOMAIN,
+});
+
+router.get('/', async function (req, res) {
+    const result = await management.users.get({
+        id: req.auth.payload.sub,
+    });
+    const {name, email, given_name, family_name, nickname, picture} = result.data;
+    console.log(`Get user info for ${email}`);
+    res.json({name, email, given_name, family_name, nickname, picture});
 });
 
 module.exports = router;
