8a2f9e0e06a84b04175aa834c0fe9110dd1bfec2
David Johnston <davidajohnston2000@gmail.com>
2025-04-16 23:05:30 +0100

    Add prettier

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,16 @@
-import {NextFunction, Request, Response} from "express";
+import { NextFunction, Request, Response } from 'express';
 
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const userInfoRouter = require('./routes/user-info');
-const {auth} = require('express-oauth2-jwt-bearer');
+const { auth } = require('express-oauth2-jwt-bearer');
 
 const jwtCheck = auth({
-    audience: 'https://user-service.dajohnston.co.uk',
-    issuerBaseURL: 'https://dajohnston.eu.auth0.com/',
-    tokenSigningAlg: 'RS256'
+  audience: 'https://user-service.dajohnston.co.uk',
+  issuerBaseURL: 'https://dajohnston.eu.auth0.com/',
+  tokenSigningAlg: 'RS256',
 });
 
 const app = express();
@@ -18,7 +18,7 @@ const app = express();
 app.use(logger('combined'));
 app.use(jwtCheck);
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Routes
@@ -26,14 +26,21 @@ app.use('/user-info', userInfoRouter);
 
 // Error handling
 // Uncaught errors returned as 500 internal server error
-app.use((error: Error, _request: Request, response: Response, _next: NextFunction) => {
+app.use(
+  (
+    error: Error,
+    _request: Request,
+    response: Response,
+    _next: NextFunction,
+  ) => {
     console.log(error.message);
-    response.status(500).json({error: error.message});
-});
+    response.status(500).json({ error: error.message });
+  },
+);
 
-// Catch-all unprocessed requests and respond with 404
+// Catch all unprocessed requests and respond with 404
 app.use((request: Request, response: Response) => {
-    response.status(404).json({error: `${request.path} Not Found`});
+  response.status(404).json({ error: `${request.path} Not Found` });
 });
 
 const port = process.env.PORT || '3000';
--- api/routes/user-info.ts
+++ api/routes/user-info.ts
@@ -1,22 +1,38 @@
-import {Request, Response} from "express";
+import { Request, Response } from 'express';
 
-const {ManagementClient} = require('auth0');
+const { ManagementClient } = require('auth0');
 const express = require('express');
 const router = express.Router();
 
 const management = new ManagementClient({
-    clientId: process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    domain: process.env.AUTH0_DOMAIN,
+  clientId: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  domain: process.env.AUTH0_DOMAIN,
 });
 
 router.get('/', async function (request: Request, response: Response) {
-    const result = await management.users.get({
-        id: request.auth?.payload.sub,
-    });
-    const {name, email, given_name, family_name, nickname, picture, user_metadata} = result.data;
-    console.log(`Get user info for ${email}`);
-    response.json({name, email, given_name, family_name, nickname, picture, user_metadata});
+  const result = await management.users.get({
+    id: request.auth?.payload.sub,
+  });
+  const {
+    name,
+    email,
+    given_name,
+    family_name,
+    nickname,
+    picture,
+    user_metadata,
+  } = result.data;
+  console.log(`Get user info for ${email}`);
+  response.json({
+    name,
+    email,
+    given_name,
+    family_name,
+    nickname,
+    picture,
+    user_metadata,
+  });
 });
 
 module.exports = router;
