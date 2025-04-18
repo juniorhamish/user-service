897f8e8f549f388d964a22d6e52f2ae89b2588bb
David Johnston <davidajohnston2000@gmail.com>
2025-04-18 16:17:53 +0100

    Create html coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
-      reporter: ['text', 'json-summary', 'json'],
+      reporter: ['text', 'json-summary', 'json', 'html'],
       reportOnFailure: true,
     },
     globals: true,
