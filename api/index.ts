29e9352b3de86e6e4fa9b0d9bfb8b5a9012bc139
David Johnston <davidajohnston2000@gmail.com>
2025-05-10 13:51:08 +0100

    Enable CORS for Swagger API generator.
    Added 'https://generator.swagger.io' to the list of allowed origins in the CORS configuration. This ensures compatibility with Swagger API testing and generation tools.

--- api/index.ts
+++ api/index.ts
@@ -22,7 +22,13 @@ app.use('/spec', express.static(path.join(path.resolve(), 'api-spec')));
 app.use(logger('combined'));
 app.use(helmet());
 app.use(
-  cors({ origin: ['https://www.dajohnston.co.uk', 'http://localhost:5173'] }),
+  cors({
+    origin: [
+      'https://www.dajohnston.co.uk',
+      'http://localhost:5173',
+      'https://generator.swagger.io',
+    ],
+  }),
 );
 app.use(jwtCheck);
 app.use(express.json());
