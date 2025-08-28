5280c6ef2c36f6f28e3e81dba765226ece81ba81
David Johnston <davidajohnston2000@gmail.com>
2025-08-29 00:05:08 +0100

    Refine CORS policy to support regex for *.vercel.app deployments

--- api/index.ts
+++ api/index.ts
@@ -26,7 +26,7 @@ app.use(
       'https://www.dajohnston.co.uk',
       'http://localhost:5173',
       'https://generator.swagger.io',
-      'https://*.vercel.app',
+      /^https:\/\/.*\.vercel\.app$/,
     ],
   }),
 );
